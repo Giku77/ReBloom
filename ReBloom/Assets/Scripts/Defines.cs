2eccba252abe41bc1f874a1b18b5ffeddab210da
길하영 <a2207624435@gmail.com>
2025-11-07 14:59:27 +0900

    BG 데이터 베이스 <-> 구글 스프레드 시트 연결 및 퀘스트 테이블 정리
    # Conflicts:
    #	ReBloom/Assets/Scenes/SampleScene.unity
    #	ReBloom/Assets/Scripts.meta

---
+++ ReBloom/Assets/Scripts/Defines.cs
@@ -0,0 +1,14 @@
+enum QuestIds
+{
+    Quest_001,
+    Quest_002,
+    Quest_003,
+    Quest_004,
+    Quest_005,
+    Quest_006,
+    Quest_007,
+    Quest_008,
+    Quest_009,
+    Quest_010,
+    Quest_011,
+}
