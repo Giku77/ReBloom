a2d55798fa0bab9ceeb97e8b4ad9a3965cc75b84
김주홍 <gicente1@nate.com>
2025-11-08 16:49:49 +0900

    add-stats-data-so

---
+++ ReBloom/Assets/Scripts/StatsData.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StatsData", menuName = "Scriptable Objects/StatsData")]
+public class StatsData : ScriptableObject
+{
+    [Header("Basics Stats")]
+    public float maxHealth = 100f;
+    public float hungerMax = 100f;
+    public float thurstMax = 100f;
+    public float pollutionMax = 100f;
+
+    [Header("Latency Reduction Rate")]
+    public float hungerDecreaseRate = 0.5f;
+    public float thirstDecreaseRate = 0.5f;
+
+    [Header("Body Temperature Related")]
+    public float normalTemperature = 36.5f;
+    public float environmentInfluenceRate = 0.1f;
+
+    [Header("Pollution Related")]
+    public float pollutionIncreaseRate = 0.5f;
+}
