df2981b0298d62e37f12ffee451170d84efa5195
GIK <a2207624435@gmail.com>
2025-11-15 23:04:29 +0900

    건축물 빌드 조건 구현중

--- ReBloom/Assets/Scripts/Build/BuildManager.cs
+++ ReBloom/Assets/Scripts/Build/BuildManager.cs
@@ -5,6 +5,8 @@ public class BuildManager : MonoBehaviour
     public static BuildManager I;
     private void Awake() => I = this;
 
+    private BuildingFootprintProvider footprintProvider;
+    private ToastMessageUI toastMessageUI;
 
     private ArcDB arcDB;
     public ArcDB ArcDB => arcDB;
@@ -14,11 +16,26 @@ public class BuildManager : MonoBehaviour
 
     public GameObject prefab;
 
+    [Header("Build Rules")]
+    [SerializeField] private LayerMask buildableLayer;
+    [SerializeField] private float maxHeightDiff = 0.1f;
+    [SerializeField] private float maxSlopeAngle = 5f;
+
+    private FlatSurfaceRule flatSurfaceRule;
+
+    private void InitRules()
+    {
+        flatSurfaceRule = new FlatSurfaceRule(buildableLayer, maxHeightDiff, maxSlopeAngle);
+    }
+
     public void Init(ArcDB arcDB, ArcRecipeDB recipeDB, GameInventory inventory)
     {
+        InitRules();
         this.arcDB = arcDB;
         this.recipeDB = recipeDB;
         this.inventory = inventory;
+        footprintProvider = GetComponent<BuildingFootprintProvider>();
+        toastMessageUI = GameObject.FindWithTag("ToastMsg").GetComponent<ToastMessageUI>();
     }
 
     public bool TryBuild(int arcId, Vector3 pos, Quaternion rot)
@@ -26,6 +43,38 @@ public class BuildManager : MonoBehaviour
         if (!arcDB.TryGet(arcId, out var arc))
         {
             Debug.LogWarning($"없는 건물: {arcId}");
+            return false;
+        }
+
+        var ctx = new ArcContext
+        {
+            Data = arc,
+            Position = pos,
+            Rotation = rot,
+            FootPrint = footprintProvider.GetFootprint(),
+            PlayerTransform = GameObject.FindWithTag("Player").transform
+        };
+
+        if (!flatSurfaceRule.Validate(ctx, out var errorCode))
+        {
+            Debug.Log($"건축 불가: {errorCode}");
+
+            switch (errorCode)
+            {
+                case "NO_FLOOR":
+                    toastMessageUI.Show("바닥이 없는 위치입니다.");
+                    break;
+                case "SLOPE_TOO_STEEP":
+                    toastMessageUI.Show("경사가 너무 가파른 곳에는 설치할 수 없습니다.");
+                    break;
+                case "NOT_FLAT":
+                    toastMessageUI.Show("평평한 지면에서만 설치할 수 있습니다.");
+                    break;
+                default:
+                    toastMessageUI.Show("이 위치에는 건축할 수 없습니다.");
+                    break;
+            }
+
             return false;
         }
 
@@ -44,7 +93,7 @@ public class BuildManager : MonoBehaviour
         return Spawn(arc, pos, rot);
     }
 
-    bool HasMaterials(ArcRecipe recipe)
+    private bool HasMaterials(ArcRecipe recipe)
     {
         foreach (var (itemId, amount) in recipe.materials)
         {
@@ -54,13 +103,13 @@ public class BuildManager : MonoBehaviour
         return true;
     }
 
-    void Consume(ArcRecipe recipe)
+    private void Consume(ArcRecipe recipe)
     {
         foreach (var (itemId, amount) in recipe.materials)
             inventory.Consume(itemId, amount);
     }
 
-    bool Spawn(ArcData arc, Vector3 pos, Quaternion rot)
+    private bool Spawn(ArcData arc, Vector3 pos, Quaternion rot)
     {
         //var prefab = Resources.Load<GameObject>($"Arc/{arc.arcId}");
         if (prefab == null)
