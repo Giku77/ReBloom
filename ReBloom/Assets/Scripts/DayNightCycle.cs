a2f60794f7dc4eb079322438b04d764cb01264ab
김주홍 <gicente1@nate.com>
2025-11-24 11:22:44 +0900

    라이트 회전 코드구현
    # Conflicts:
    #	ReBloom/Assets/Scenes/MainScene.unity
    #	ReBloom/Assets/TextMesh Pro/Resources/Fonts & Materials/BinggraeII SDF.asset

---
+++ ReBloom/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [Header ("Light")]
+    [SerializeField] private Light sun;
+    [SerializeField] private float dayLengthInSeconds = 2160f;
+    [SerializeField] private float currentTime = 800f;
+
+
+    private void Start()
+    {
+        currentTime = 800f;
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        float t = (currentTime / dayLengthInSeconds) % 1f;
+
+        sun.transform.rotation = Quaternion.Euler(t * 360f - 90f, 0f, 0f);
+    }
+}
