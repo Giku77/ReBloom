4b3bccb33134089fe74e9a3c41684829e0e03406
김주홍 <gicente1@nate.com>
2025-12-02 17:57:39 +0900

    타이틀씬 UI 및 구조 생성

---
+++ ReBloom/Assets/Scripts/Title/MainTitleWindow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainTitleWindow : Window
+{
+    public void OnGameStartButtonClicked()
+    {
+        SceneManager.LoadSceneAsync("MainScene");
+    }
+}
