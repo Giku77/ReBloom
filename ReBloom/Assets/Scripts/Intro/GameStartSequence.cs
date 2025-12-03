95f12eea2f13f1451b551eff3b645c8e0b4b5e97
김주홍 <gicente1@nate.com>
2025-12-03 11:52:50 +0900

    게임 시작 후 플레이어 일어나는 연출 추가

---
+++ ReBloom/Assets/Scripts/Intro/GameStartSequence.cs
@@ -0,0 +1,53 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class GameStartSequence : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private TutorialManager tutorialManager;
+
+    [Header("Settings")]
+    [SerializeField] private float initialDelay = 1f;
+
+    private void Awake()
+    {
+        if (tutorialManager != null)
+        {
+            tutorialManager.enabled = false;
+        }
+    }
+
+    private async void Start()
+    {
+        await PlaySequence();
+    }
+
+    public async UniTask PlaySequence()
+    {
+        if (playerController != null)
+        {
+            playerController.SetBlocked(true);
+        }
+
+        playerController.Anim.PlaySleep();
+
+        await UniTask.Delay(5000);
+
+        playerController.Anim.PlayStandUp();
+
+        await UniTask.Delay((int)(initialDelay * 1000));
+
+        if (tutorialManager != null)
+        {
+            tutorialManager.enabled = true;
+        }
+        else
+        {
+            if (playerController != null)
+            {
+                playerController.SetBlocked(false);
+            }
+        }
+    }
+}
