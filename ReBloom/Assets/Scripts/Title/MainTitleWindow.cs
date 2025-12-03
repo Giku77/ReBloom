a74d7ddeb9d48eb21454e5c70e602eb384874ce0
김주홍 <gicente1@nate.com>
2025-12-03 09:38:21 +0900

    타이틀 미구현 요소 알람 추가 및 메인씬 초기화 시 널 체크 수정

--- ReBloom/Assets/Scripts/Title/MainTitleWindow.cs
+++ ReBloom/Assets/Scripts/Title/MainTitleWindow.cs
@@ -1,11 +1,52 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Triggers;
+using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainTitleWindow : Window
 {
+    [SerializeField] private TextMeshProUGUI toastMessage;
+
+    private void Start()
+    {
+        toastMessage.gameObject.SetActive(false);
+    }
+
+
     public void OnGameStartButtonClicked()
     {
         SceneManager.LoadScene("LoadingScene");
+    }
+
+    public void OnLoadGameButtonClicekd()
+    { 
+        OnNotImplementedButtonClickeddAsync().Forget();
+    }
+
+    public void OnSettingButtonClicked()
+    {
+        OnNotImplementedButtonClickeddAsync().Forget();
+    }
+
+    private async UniTask OnNotImplementedButtonClickeddAsync()
+    {
+        toastMessage.gameObject.SetActive(true);
+
+        toastMessage.text = "추후 구현 예정입니다.";
+
+        await UniTask.Delay(2000);
+
+        toastMessage.gameObject.SetActive(false);
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
