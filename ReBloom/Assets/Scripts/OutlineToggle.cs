1f51989d66696cfe5a287ce045d75d24045b8f3e
길하영 <a2207624435@gmail.com>
2025-12-11 17:58:44 +0900

    상호작용 아웃라인 작업중

---
+++ ReBloom/Assets/Scripts/OutlineToggle.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class OutlineToggle : MonoBehaviour
+{
+    [SerializeField] private bool outlined = false;
+
+    [SerializeField] private uint outlineLayerMask = 1u << 8; // 8번 레이어로 설정
+
+    private Renderer[] renderers;
+    private uint originalMask;
+
+    private void Awake()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+        if (renderers.Length > 0)
+            originalMask = renderers[0].renderingLayerMask;
+
+        Apply();
+    }
+
+// #if UNITY_EDITOR
+//     private void OnValidate()
+//     {
+//         if (!Application.isPlaying)
+//             renderers = GetComponentsInChildren<Renderer>();
+
+//         Apply();
+//     }
+// #endif
+
+    private void Apply()
+    {
+        if (renderers == null) return;
+
+        foreach (var r in renderers)
+        {
+            uint mask = outlined
+                ? (originalMask | outlineLayerMask)   // 켜기
+                : (originalMask & ~outlineLayerMask); // 끄기
+
+            r.renderingLayerMask = mask;
+        }
+    }
+
+    public void SetOutlined(bool value)
+    {
+        outlined = value;
+        Apply();
+    }
+}
